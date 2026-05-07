e073f969fbfa5f063f7202f76227386540fe3c2e
Anil <anilmende2002@gmail.com>
2026-05-07 23:47:43 +0530

    added topic model

---
+++ backend/models/topicModel.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+const topicSchema = new mongoose.Schema({
+
+    subjectId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Subject",
+        required : true
+    },
+    
+    userId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+
+    title : {
+        type : String,
+        required : true
+    },
+
+    status : {
+        type : String,
+        enum : ["pending", "completed", "revision"],
+        default : "pending"
+    },
+
+    priority : {
+        type : String,
+        enum : ["low", "medium", "high"],
+        default : "medium"
+    },
+
+    plannedDate : {
+        type : Date
+    },
+
+    completedDate : {
+        type : Date
+    },
+
+    revisionCount : {
+        type : Number,
+        default : 0
+    },
+
+    isDeleted : {
+        type : Boolean,
+        default : false
+    }
+
+}, { timestamps : true });
+
+topicSchema.index({ userId : 1});
+topicSchema.index({ subjectId : 1 });
+topicSchema.index({ status : 1 });
+
+const Topic = mongoose.model("Topic", topicSchema);
+
+export default Topic;
