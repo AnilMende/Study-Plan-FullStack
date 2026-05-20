cf2067f9677bb1d8ce885147e0f0310e9d776aef
Anil <anilmende2002@gmail.com>
2026-05-20 20:34:17 +0530

    automatic token refresh system

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,4 +6,91 @@ const api = axios.create({
     withCredentials : true
 });
 
+// Refresh token request state
+let isRefreshing = false;
+
+// Queue for failed requests
+let failedQueue = [];
+
+// Process queue requests
+const processQueue = (error) => {
+
+    failedQueue.forEach((promise) => {
+
+        if(error){
+            promise.reject(error)
+        } else{
+            promise.resolve();
+        }
+    });
+
+    failedQueue = [];
+}
+
+// Response interceptor
+api.interceptors.response.use(
+
+    // Success
+    (response) =>  response,
+
+    // Error
+    async (error) => {
+
+        const originalRequest = error.config;
+
+        // if access token expired
+        if(error.response?.status === 401 && !originalRequest._retry){
+
+            // prevent infinite retry loop
+            originalRequest._retry = true;
+
+            // if refresh already happening
+            if(isRefreshing){
+
+                return new Promise(
+                    (resolve, reject) => {
+
+                        failedQueue.push({
+                            resolve,
+                            reject
+                        })
+                    }
+                ).then(() => {
+
+                    return api(originalRequest);
+                });
+            }
+
+            isRefreshing = true;
+
+            try {
+                
+                // call refresh endpoint
+                await api.post("/auth/refresh-token");
+
+                // Retry all queue requests
+                processQueue(null);
+
+                // Retry original request
+                return api(originalRequest);
+
+            } catch (refreshError) {
+                
+                processQueue(refreshError);
+
+                // Logout situation
+                window.location.href = "/login";
+
+                return Promise.reject(refreshError);
+            } finally{
+
+                isRefreshing = false;
+
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export default api;
